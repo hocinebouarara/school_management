03a3eda12fbbce6b9cb03aa79f2b64320e74dc16
hocinebouarara <hocine@univ-tiaret.dz>
2022-03-12 00:29:17 +0100

    create links class for add sources of views

---
+++ school_management/src/helpres/Links.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpres;
+
+/**
+ *
+ * @author hocin
+ */
+public class Links {
+
+    public static String LOGINVIEW = "/login/loginView.fxml";
+    
+    public static String DASHBOARD = "/dashboard/FXML.fxml";
+
+    public static String PROFILEIMAGE = "/images/myImage.jpg";
+
+}
