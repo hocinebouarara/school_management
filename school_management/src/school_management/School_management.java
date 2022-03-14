394f8c2427a74ede543e0fdfa952ecfe064c1292
hocinebouarara <hocine@univ-tiaret.dz>
2022-03-14 03:54:19 +0100

    update the window decor

--- school_management/src/school_management/School_management.java
+++ school_management/src/school_management/School_management.java
@@ -10,7 +10,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,9 +22,11 @@ public class School_management extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(Links.LOGINVIEW));
+Parent root = FXMLLoader.load(getClass().getResource(Links.DASHBOARD));
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.DECORATED);
         stage.show();
     }
 
