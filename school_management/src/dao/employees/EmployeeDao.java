b3435dbfef1e444b452fab91bc198697e421b248
hocinebouarara <hocine@univ-tiaret.dz>
2022-03-16 22:05:33 +0100

    create interface of employees for data access object

---
+++ school_management/src/dao/employees/EmployeeDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao.employees;
+
+import java.util.List;
+import models.Employee;
+
+/**
+ *
+ * @author hocin
+ */
+public interface EmployeeDao {
+    
+    
+    List<Employee> findAll();
+    
+    Employee findById(int id);
+    
+    void save(Employee employee);
+    
+    void deleteById(int id);
+    
+}
